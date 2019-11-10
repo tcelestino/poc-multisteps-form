26a060a9ced0da2cf0bd228f43cdab76b20ad11b
Tiago Celestino <tcelestino@gmail.com>
2019-11-10 10:29:59 -0300

    refactor(formcontext): remove code unnecessary

--- contexts/FormContext.jsx
+++ contexts/FormContext.jsx
@@ -1,17 +1,7 @@
 import React, { createContext } from 'react';
 import { Form } from '@catho/quantum/Form';
 
-const state = {
-  currentStep: 1
-};
-
-const handleOnStep = currentStep => {
-  return currentStep + 1;
-};
-
-const FormContext = createContext({
-  currentStep: 1
-});
+const FormContext = createContext();
 
 function FormProvider(props) {
   const { children } = props;
