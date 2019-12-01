5a69b7f5050d28f7f723925436eb9a6e3012a320
Tiago Celestino <tcelestino@gmail.com>
2019-12-01 20:08:29 -0300

    feat(next.config.js): create public envs variables
    #6

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ require('dotenv').config();
 module.exports = {
   serverRuntimeConfig: {
     API_HOST: process.env.API_HOST
+  },
+  publicRuntimeConfig: {
+    STATES: process.env.STATES,
+    CITIES: process.env.CITIES
   }
 }
